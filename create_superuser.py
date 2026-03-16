e94af6bbf4a57435033d5e6049df3139491a1f3e
Henry Dorsett Case(Ataberk Yakar) <Ataberk.Yakar@edu.rtu.lv>
2026-03-16 14:36:39 +0300

    update

--- create_superuser.py
+++ create_superuser.py
@@ -1,4 +1,4 @@
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'password')
+    User.objects.create_superuser('ataberkyakar', 'ataberkyakar@gmail.com', '87818781')
