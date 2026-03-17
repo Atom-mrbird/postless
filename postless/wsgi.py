dcebc9318d1ab9e29c423d13c3507660c11e8ca3
Henry Dorsett Case(Ataberk Yakar) <Ataberk.Yakar@edu.rtu.lv>
2026-03-17 14:36:42 +0300

    fix

--- postless/wsgi.py
+++ postless/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for postless project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import django
 
-from django.core.wsgi import get_wsgi_application
+os.environ["DJANGO_SETTINGS_MODULE"] = "postless.settings"  # ← replace with your actual settings module
+django.setup()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'postless.settings')
+from django.contrib.auth import get_user_model
 
-application = get_wsgi_application()
+User = get_user_model()
