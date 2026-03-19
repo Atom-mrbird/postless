7a7f0d6bb0605036a93525139c8e7ab2456544cc
Henry Dorsett Case(Ataberk Yakar) <ataberkyakar@outlook.com>
2026-03-19 14:18:03 +0300

    Update settings.py

--- postless/settings.py
+++ postless/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-vf*0g94mj7j_m$0@)edp6%ffp$rlj*0efoxgcfe&z+u!!7=odd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'www.postless.solutions',
