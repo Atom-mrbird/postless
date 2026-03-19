52744395bbde258ee401282585ae0cc3dc37f7ac
Henry Dorsett Case(Ataberk Yakar) <ataberkyakar@outlook.com>
2026-03-19 17:01:58 +0300

    Update settings.py

--- postless/settings.py
+++ postless/settings.py
@@ -34,7 +34,8 @@ ALLOWED_HOSTS = [
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://postless.solutions',
-    'https://www.postless.solutions'
+    'https://www.postless.solutions',
+    'https://plankton-app-wjaj8.ondigitalocean.app/'
 ]
 APPEND_SLASH=False
 # Session and Cookie Settings for Ngrok/HTTPS
