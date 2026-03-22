f65186c9562c00f13945c5a611b2ebb7bc8aaa14
Henry Dorsett Case(Ataberk Yakar) <Ataberk.Yakar@edu.rtu.lv>
2026-03-22 19:24:34 +0300

    fix

--- postless/asgi.py
+++ postless/asgi.py
@@ -10,13 +10,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'postless.settings')
 
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     # "websocket": AuthMiddlewareStack(
     #     URLRouter(
     #         # websocket_urlpatterns
